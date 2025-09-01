8c38d5b94888c33cdaa717dda4ba068860a092ec
daun-gatal <rsudo54@gmail.com>
2025-09-02 05:53:53 +0700

    Update main.tf

--- nessie/output.tf
+++ nessie/output.tf
@@ -1,4 +1,9 @@
-# output "nessie_service_dns" {
-#   value       = 
-#   description = "The Nessie service DNS name"
-# }
+output "nessie_service_dns" {
+  value       = "${helm_release.nessie.name}.${kubernetes_namespace.nessie.metadata[0].name}.svc.cluster.local"
+  description = "The Nessie service DNS name"
+}
+
+output "nessie_service_port" {
+  value       = 19120
+  description = "The Nessie service port"
+}
