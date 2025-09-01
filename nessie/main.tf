801ac111641e6b612cbadcbc6a858ec811688e41
daun-gatal <rsudo54@gmail.com>
2025-09-01 17:02:51 +0700

    Add nessie module

---
+++ nessie/main.tf
@@ -0,0 +1,147 @@
+locals {
+  prefix = var.prefix
+  release_name = "${local.prefix}-release"
+  secret_name = "${local.prefix}-secret"
+  s3_secret_name = "${local.prefix}-s3-secret"
+  s3_warehouse_location = "s3://${var.nessie_s3_bucket}/${var.nessie_default_warehouse}"
+}
+
+resource "kubernetes_namespace" "nessie" {
+  metadata {
+    name = var.namespace
+  }
+}
+
+resource "kubernetes_secret" "nessie_jdbc" {
+  metadata {
+    name      = local.secret_name
+    namespace = kubernetes_namespace.nessie.metadata[0].name
+  }
+
+  data = {
+    username = base64encode(var.nessie_jdbc_username)
+    password = base64encode(var.nessie_jdbc_password)
+  }
+
+  type = "Opaque"
+}
+
+resource "kubernetes_secret" "nessie_s3" {
+  metadata {
+    name      = local.s3_secret_name
+    namespace = kubernetes_namespace.nessie.metadata[0].name
+  }
+
+  data = {
+    id = base64encode(var.nessie_s3_access_key_name)
+    secret = base64encode(var.nessie_s3_access_key_secret)
+  }
+
+  type = "Opaque"
+}
+
+resource "helm_release" "nessie" {
+  name       = local.release_name
+  namespace  = kubernetes_namespace.nessie.metadata[0].name
+  repository = "https://charts.projectnessie.org"
+  chart      = var.chart_name
+  version    = var.chart_version
+
+  set = [
+    {
+      name  = "versionStoreType"
+      value = "JDBC"
+    },
+    {
+        name = "catalog.enabled"
+        value = "true"
+    },
+    {
+        name = "catalog.iceberg.defaultWarehouse"
+        value = var.nessie_default_warehouse
+    },
+    {
+        name = "catalog.warehouses[0].name"
+        value = var.nessie_default_warehouse
+    },
+    {
+        name = "catalog.warehouses[0].location"
+        value = local.s3_warehouse_location
+    },
+    {
+        name = "storage.s3.defaultOptions.region"
+        value = var.nessie_s3_region
+    },
+    {
+        name = "storage.s3.defaultOptions.endpoint"
+        value = var.nessie_s3_endpoint
+    },
+    {
+        name = "storage.s3.defaultOptions.pathStyleAccess"
+        value = "true"
+    },
+    {
+        name = "storage.s3.defaultOptions.accessKeySecret.name"
+        value = local.s3_secret_name
+    },
+    {
+        name = "storage.s3.defaultOptions.accessKeySecret.awsAccessKeyId"
+        value = "id"
+    },
+    {
+        name = "storage.s3.defaultOptions.secretKeySecret.awsSecretAccessKey"
+        value = "secret"
+    },
+    {
+        name = "metrics.enabled"
+        value = "false"
+    },
+    {
+        name = "serviceMonitor.enabled"
+        value = "false"
+    },
+    {
+        name = "ingress.enabled"
+        value = "true"
+    },
+    {
+        name = "ingress.className"
+        value = "tailscale"
+    },
+    {
+        name = "ingress.pathType"
+        value = "Prefix"
+    },
+    {
+        name = "ingress.hosts[0].host"
+        value = "${local.prefix}.local"
+    },
+    {
+        name = "ingress.hosts[0].paths[0]"
+        value = "/"
+    },
+    {
+        name = "ingress.tls[0].hosts[0]"
+        value = "${local.prefix}.local"
+    }
+  ]
+
+  set_sensitive = [
+    {
+        name = "jdbc.jdbcUrl"
+        value = "jdbc:postgresql://${var.nessie_jdbc_url}:${var.nessie_jdbc_port}/${var.nessie_database_name}currentSchema=public"
+    },
+    {
+        name = "jdbc.secret.name"
+        value = local.secret_name
+    },
+    {
+        name = "jdbc.secret.username"
+        value = "username"
+    },
+    {
+        name = "jdbc.secret.password"
+        value = "password"
+    }
+  ]
+}
