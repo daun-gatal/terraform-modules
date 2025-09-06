8d6644444af6c8bc4fe2e30f072c21d7fb29122d
daun-gatal <rsudo54@gmail.com>
2025-09-06 09:43:58 +0700

    update main.tf

--- airflow/main.tf
+++ airflow/main.tf
@@ -70,7 +70,7 @@ resource "helm_release" "airflow" {
         args = [
           "bash",
           "-c",
-          "if [ \"$AIRFLOW__LOGGING__REMOTE_LOGGING\" = \"True\" ]; then airflow connections add minio_conn --conn-type 's3' --conn-login '${var.aws_access_key_id}' --conn-password '${var.aws_secret_access_key}' --conn-extra \"{\\\"region_name\\\": \\\"${var.aws_region}\\\", \\\"endpoint_url\\\": \\\"${var.aws_endpoint_url}\\\"}\" || true; fi; exec airflow scheduler"
+          "if [ \"$AIRFLOW__LOGGING__REMOTE_LOGGING\" = \"True\" ]; then airflow connections add minio_conn --conn-type 'aws' --conn-login '${var.aws_access_key_id}' --conn-password '${var.aws_secret_access_key}' --conn-extra \"{\\\"region_name\\\": \\\"${var.aws_region}\\\", \\\"endpoint_url\\\": \\\"${var.aws_endpoint_url}\\\"}\" || true; fi; exec airflow scheduler"
         ]
       }
     }),
