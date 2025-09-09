e07fcf4a27e4214c8516304f8b39b237cf755547
daun-gatal <rsudo54@gmail.com>
2025-09-10 04:38:30 +0700

    update variable

--- spark/scripts/spark-connect-server.sh
+++ spark/scripts/spark-connect-server.sh
@@ -5,17 +5,38 @@
 
 set -euo pipefail
 
-echo "Starting Spark Connect Server..."
+SPARK_HOME=${SPARK_HOME:-/opt/spark}
+
+# Detect Spark version
+SPARK_VERSION=$($SPARK_HOME/bin/spark-submit --version 2>&1 | \
+  grep -oE "version [0-9]+\.[0-9]+\.[0-9]+" | awk '{print $2}')
+echo "Detected Spark version: $SPARK_VERSION"
+
+SPARK_MAJOR=$(echo "$SPARK_VERSION" | cut -d. -f1)
+
 echo "Master URL: ${master_url}"
 echo "Executor Memory: ${executor_memory}"
 echo "Executor Cores: ${executor_cores}"
 echo "Max Cores: ${max_cores}"
 
-exec /opt/spark/bin/spark-submit \
-  --class org.apache.spark.sql.connect.service.SparkConnectServer \
-  --name 'Spark Connect Server' \
-  --master "${master_url}" \
-  --conf "spark.executor.memory=${executor_memory}" \
-  --conf "spark.executor.cores=${executor_cores}" \
-  --conf "spark.cores.max=${max_cores}" \
-  --conf "spark.dynamicAllocation.enabled=false"
+if [ "$SPARK_MAJOR" -lt 4 ]; then
+  echo "Using Spark < 4. Adding spark-connect package..."
+  exec $SPARK_HOME/bin/spark-submit \
+    --class org.apache.spark.sql.connect.SparkConnectServer \
+    --packages "org.apache.spark:spark-connect_2.12:${SPARK_VERSION}" \
+    --master "${master_url}" \
+    --conf "spark.executor.memory=${executor_memory}" \
+    --conf "spark.executor.cores=${executor_cores}" \
+    --conf "spark.cores.max=${max_cores}" \
+    --conf "spark.dynamicAllocation.enabled=false"
+else
+  echo "Using Spark >= 4. Running current Spark Connect script..."
+  exec $SPARK_HOME/bin/spark-submit \
+    --class org.apache.spark.sql.connect.service.SparkConnectServer \
+    --name 'Spark Connect Server' \
+    --master "${master_url}" \
+    --conf "spark.executor.memory=${executor_memory}" \
+    --conf "spark.executor.cores=${executor_cores}" \
+    --conf "spark.cores.max=${max_cores}" \
+    --conf "spark.dynamicAllocation.enabled=false"
+fi
