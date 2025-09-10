34eaca5d439c4ca6fdd365884a700bd2d661c3c1
daun-gatal <rsudo54@gmail.com>
2025-09-10 07:20:50 +0700

    update variable

--- spark/scripts/spark-connect-server.sh
+++ spark/scripts/spark-connect-server.sh
@@ -5,38 +5,17 @@
 
 set -euo pipefail
 
-SPARK_HOME=$${SPARK_HOME:-/opt/spark}
-
-# Detect Spark version
-SPARK_VERSION=$$($SPARK_HOME/bin/spark-submit --version 2>&1 | \
-  grep -oE "version [3-9]\.[0-9]+\.[0-9]+" | head -n1 | awk '{print $2}')
-echo "Detected Spark version: $SPARK_VERSION"
-
-SPARK_MAJOR=$$(echo "$SPARK_VERSION" | cut -d. -f1)
-
+echo "Starting Spark Connect Server..."
 echo "Master URL: ${master_url}"
 echo "Executor Memory: ${executor_memory}"
 echo "Executor Cores: ${executor_cores}"
 echo "Max Cores: ${max_cores}"
 
-if [ "$SPARK_MAJOR" -lt 4 ]; then
-  echo "Using Spark < 4. Adding spark-connect package..."
-  exec "$SPARK_HOME/sbin/start-connect-server.sh" \
-    --packages "org.apache.spark:spark-connect_2.12:$${SPARK_VERSION}" 
-    # --master "${master_url}" \
-    # --name 'Spark Connect Server' \
-    # --conf "spark.executor.memory=${executor_memory}" \
-    # --conf "spark.executor.cores=${executor_cores}" \
-    # --conf "spark.cores.max=${max_cores}" \
-    # --conf "spark.dynamicAllocation.enabled=false"
-else
-  echo "Using Spark >= 4. Running current Spark Connect script..."
-  exec $SPARK_HOME/bin/spark-submit \
-    --class org.apache.spark.sql.connect.service.SparkConnectServer \
-    --name 'Spark Connect Server' \
-    --master "${master_url}" \
-    --conf "spark.executor.memory=${executor_memory}" \
-    --conf "spark.executor.cores=${executor_cores}" \
-    --conf "spark.cores.max=${max_cores}" \
-    --conf "spark.dynamicAllocation.enabled=false"
-fi
+exec /opt/spark/bin/spark-submit \
+  --class org.apache.spark.sql.connect.service.SparkConnectServer \
+  --name 'Spark Connect Server' \
+  --master "${master_url}" \
+  --conf "spark.executor.memory=${executor_memory}" \
+  --conf "spark.executor.cores=${executor_cores}" \
+  --conf "spark.cores.max=${max_cores}" \
+  --conf "spark.dynamicAllocation.enabled=false"
