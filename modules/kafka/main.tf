f60b78fc5754b849d3eb4d6044bfa75cc920ca3e
daun-gatal <rsudo54@gmail.com>
2025-09-14 17:56:59 +0700

    update main.tf

--- modules/kafka/main.tf
+++ modules/kafka/main.tf
@@ -91,7 +91,7 @@ resource "kubernetes_manifest" "kafka_cluster" {
         }
 
         template = {
-          externalBootstrapService = {
+          bootstrapService = {
             metadata = {
               annotations = {
                 "tailscale.com/expose"   = tostring(var.tailscale_expose)
--- modules/kafka/output.tf
+++ modules/kafka/output.tf
@@ -1,4 +1,4 @@
-output "kafka_bootstrap_servers" {
+output "kafka_int_bootstrap_servers" {
   description = "Kafka bootstrap servers connection string for client applications"
   value       = "${local.prefix}-kafka-bootstrap.${var.namespace}.svc.cluster.local:9092"
 }
