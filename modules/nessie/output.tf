c5355cbf2ca18477e0f848f151ba7e428846cfb7
daun-gatal <rsudo54@gmail.com>
2025-09-14 16:10:50 +0700

    update main.tf

--- modules/nessie/output.tf
+++ modules/nessie/output.tf
@@ -1,5 +1,5 @@
 output "nessie_service_dns" {
-  value       = "${local.release_name}.${kubernetes_namespace.nessie.metadata[0].name}.svc.cluster.local"
+  value       = "${local.release_name}.${var.namespace}.svc.cluster.local"
   description = "The Nessie service DNS name"
 }
 
--- modules/trino/output.tf
+++ modules/trino/output.tf
@@ -1,6 +1,6 @@
 output "trino_service_dns" {
   description = "The DNS name of the Trino service"
-  value       = "${local.release_name}.${kubernetes_namespace.trino.metadata[0].name}.svc.cluster.local" 
+  value       = "${local.release_name}.${var.namespace}.svc.cluster.local" 
 }
 
 output "trino_service_port" {
