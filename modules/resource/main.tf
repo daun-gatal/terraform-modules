8e9e60b4a5832a3d2352ef0cd6b1d6c280076d6c
daun-gatal <rsudo54@gmail.com>
2025-09-17 18:56:43 +0700

    update main.tf

--- modules/resource/main.tf
+++ modules/resource/main.tf
@@ -1,24 +1,15 @@
-# Create a LimitRange for the namespace to set default CPU/Memory
-resource "kubernetes_limit_range" "namespace_limits" {
+# Create a ResourceQuota for the namespace to limit resource consumption
+resource "kubernetes_resource_quota" "namespace_quota" {
   metadata {
-    name      = "${var.namespace}-limit-range"
+    name      = "${var.namespace}-resource-quota"
     namespace = var.namespace
   }
 
   spec {
-    limit {
-      type = "Pod"
-
-      # If a Pod/Container doesn't specify resources, these will be applied
-      default = {
-        cpu    = var.cpu
-        memory = var.memory
-      }
-
-      default_request = {
-        cpu    = var.cpu
-        memory = var.memory
-      }
+    hard = {
+      # Total namespace limits only (more flexible)
+      "cpu"    = var.cpu
+      "memory" = var.memory
     }
   }
 }
