94c430263cedc2152e7bd01bc0093eb34c822276
daun-gatal <rsudo54@gmail.com>
2025-09-30 06:06:58 +0700

    Add limit range

--- modules/resource/main.tf
+++ modules/resource/main.tf
@@ -1,3 +1,23 @@
+# Create a LimitRange to set default pod requests/limits in the namespace
+resource "kubernetes_limit_range" "namespace_limits" {
+  metadata {
+    name      = "${var.namespace}-limit-range"
+    namespace = var.namespace
+  }
+
+  spec {
+    limit {
+      type = "Pod"
+
+      # Default values applied when pods don't specify resources
+      default = {
+        cpu    = var.cpu
+        memory = var.memory
+      }
+    }
+  }
+}
+
 # Create a ResourceQuota for the namespace to limit resource consumption
 resource "kubernetes_resource_quota" "namespace_quota" {
   metadata {
@@ -7,9 +27,11 @@ resource "kubernetes_resource_quota" "namespace_quota" {
 
   spec {
     hard = {
-      # Total namespace limits only (more flexible)
+      # Namespace-wide hard caps
       "cpu"    = var.cpu
       "memory" = var.memory
     }
   }
+
+  depends_on = [ kubernetes_limit_range.namespace_limits ]
 }
