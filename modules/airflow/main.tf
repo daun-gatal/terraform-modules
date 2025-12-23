c2c131416b7d353a69b032fb96c7ce21f0910f29
daun-gatal <rsudo54@gmail.com>
2025-12-23 05:02:06 +0000

    Edit main.tf

--- modules/airflow/main.tf
+++ modules/airflow/main.tf
@@ -425,5 +425,7 @@ resource "helm_release" "airflow" {
   depends_on = [kubernetes_secret.airflow_secret]
 
   recreate_pods = true
+  force_update  = true
+  wait          = true
   timeout       = 600
 }
