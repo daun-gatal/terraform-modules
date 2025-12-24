bd101eca8c13de35370637675c5f723321d35786
daun-gatal <rsudo54@gmail.com>
2025-12-24 06:50:21 +0000

    Edit output.tf

--- modules/trino/output.tf
+++ modules/trino/output.tf
@@ -7,8 +7,3 @@ output "trino_service_port" {
   description = "The port of the Trino service"
   value       = 8080
 }
-
-output "trino_acl" {
-  description = "The access control in JSON format"
-  value       = try(data.kubernetes_config_map.trino_acl.data["rules.json"], "{}")
-}
